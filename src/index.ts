94a000385555b7c22e2e4a638c25771c87618c41
Banks Nussman <banks@nussman.us>
2021-03-18 14:43:31 -0400

    ?

--- src/index.ts
+++ src/index.ts
@@ -6,174 +6,91 @@ import { initializeSentry } from "./utils/sentry";
 import db from "./utils/db";
 import { ObjectId } from "mongodb";
 
+class QueueItem {
+    public rider: string;
+    public origin: string;
+    public destination: string;
+    public groupSize: number;
+    public isAccepted: boolean;
+    public state: number;
+    public timestamp: number;
+
+    constructor(rider: string, origin: string, destination: string, groupSize: number) {
+        this.rider = rider;
+        this.origin = origin;
+        this.destination = destination;
+        this.groupSize = groupSize;
+        this.isAccepted = false;
+        this.state = 0;
+        this.timestamp = Date.now();
+    }
+}
+
+export class Queue {
+    private queue: QueueItem[]
+
+    constructor() {
+        this.queue = [];
+    }
+
+    public join(entry: QueueItem) {
+        this.queue.push(entry);
+    }
+    
+    public accept(rider: string) {
+        for (let i = 0; i < this.queue.length; i++) {
+            if (this.queue[i].rider === rider) {
+                this.queue[i].isAccepted = true;
+            }
+        }
+    }
+}
+
+export class QueueManger {
+
+    private queues: Map<string, Queue>;
+
+    constructor() {
+        this.queues = new Map();
+    }
+
+    public create(beeper: string) {
+       this.queues.set(beeper, new Queue()); 
+    }
+
+    public get(beeper: string): Queue | undefined {
+        return this.queues.get(beeper);
+    }
+
+    public remove(beeper: string): boolean {
+        return this.queues.delete(beeper);
+    }
+}
+
+
 const server = new Server();
+const manager = new QueueManger();
+const clients: Map<string, Socket> = new Map();
+const userToSocketMapping: Map<string, string> = new Map();
 
-initializeSentry();
+server.on('connection', function (socket: Socket) {
 
-server.on("connection", function (socket: Socket) {
-    socket.on('getRiderStatus', async function (authToken: string, beepersID: string) {
-        const userid = await isTokenValid(authToken);
+    socket.on('auth', async function (authToken: string) {
+        const userId = await isTokenValid(authToken);
 
-        if (!userid) {
-            server.to(socket.id).emit('updateRiderStatus', makeJSONError("Your token is not valid."));
-            return;
-        }
+        if (!userId) return console.log("Auth Failed");
 
-        if (!beepersID) {
-            server.to(socket.id).emit('updateRiderStatus', makeJSONError("You did not provide a beeper's id"));
-            return;
-        }
+        userToSocketMapping.set(socket.id, userId);
 
-        const filter = [{ $match: {'fullDocument.beeper': new ObjectId(beepersID) } }];
-        const stream = db.beep().collection('queue-entry').watch(filter, { fullDocument: 'updateLookup' });
-
-        stream.on("change", (changeEvent) => {
-            server.to(socket.id).emit("updateRiderStatus");
-            console.log("Rider Update", userid);
-
-        });
-
-        socket.on('stopGetRiderStatus', function stop() {
-            stream.close();
-        });
-
-        socket.on("disconnect", () => {
-            stream.close();
-            socket.removeAllListeners();
-        });
-
-        const filter2 = [{ $match: {'fullDocument.user': new ObjectId(beepersID) } }];
-        const stream2 = db.beep().collection('location').watch(filter2, { fullDocument: 'updateLookup' });
-
-        stream2.on("change", (changeEvent) => {
-            console.log("Sending Location Update to Rider:", changeEvent);
-            //@ts-ignore
-            server.to(socket.id).emit("hereIsBeepersLocation", changeEvent.fullDocument);
-
-        });
-
-        socket.on('stopGetRiderStatus', function stop() {
-            stream2.close();
-        });
-
-        socket.on("disconnect", () => {
-            stream2.close();
-            socket.removeAllListeners();
-        });
+        clients.set(userId, socket);
     });
 
-    socket.on('getQueue', async function (userid: string) {
-        const filter = [{ $match: {'fullDocument.beeper': new ObjectId(userid) } }];
-        //const stream = db.beep().collection('queue-entry').watch(filter);
-        const stream = db.beep().collection('queue-entry').watch(filter, { fullDocument: 'updateLookup' });
+    socket.on('disconnect', () => {
+        const userId = userToSocketMapping.get(socket.id);
 
-        stream.on("change", (changeEvent) => {
+        if (!userId) throw new Error("User not found in userToSocketMapping");
 
-            server.to(socket.id).emit("updateQueue");
-
-        });
-
-        socket.on('stopGetQueue', function stop() {
-            stream.close();
-        });
-
-        socket.on("disconnect", () => {
-            stream.close();
-            socket.removeAllListeners();
-        });
-    });
-
-    socket.on('getUser', async function (authToken: string) {
-
-        const userid = await isTokenValid(authToken);
-
-        db.beep().collection('user').findOne(
-            { _id: userid },
-            { projection: {
-                _id: false,
-                first: true,
-                last: true,
-                email: true,
-                phone: true,
-                venmo: true,
-                isBeeping: true,
-                isEmailVerified: true,
-                isStudent: true,
-                masksRequired: true,
-                groupSize: true,
-                singlesRate: true,
-                groupRate: true,
-                photoUrl: true,
-                username: true
-            }},
-            function(err, result) {
-                console.log(result);
-                server.to(socket.id).emit('updateUser', result);
-            }
-        );
-
-
-
-        if (!userid) {
-            server.to(socket.id).emit('updateUser', makeJSONError("Your token is not valid."));
-            return;
-        }
-
-        const filter = [{ $match: { "fullDocument._id": userid}  }];
-        const stream = db.beep().collection('user').watch(filter, { fullDocument: 'updateLookup' });
-
-        stream.on("change", (changeEvent) => {
-            //@ts-ignore
-            if (changeEvent.updateDescription?.updatedFields) {
-                //@ts-ignore
-                console.log("User Profile Update", userid, changeEvent.updateDescription.updatedFields);
-                //@ts-ignore
-                if (!changeEvent.updateDescription.updatedFields.password) {
-                    //@ts-ignore
-                    server.to(socket.id).emit('updateUser', changeEvent.updateDescription.updatedFields);
-                }
-            }
-
-        });
-
-        socket.on('stopGetUser', function stop() {
-            stream.close();
-        });
-
-        socket.on("disconnect", () => {
-            stream.close();
-            socket.removeAllListeners();
-        });
-    });
-
-    socket.on('updateUsersLocation', async function (authToken: string, latitude: number, longitude: number, altitude: number, accuracy: number, altitudeAccuracy: number, heading: number, speed: number) {
-        const userid = await isTokenValid(authToken);
-
-        if (!userid) {
-            return console.log("Token is not valid. Just skipping this entry attempt");
-        }
-
-        const dataToInsert = {
-            user: new ObjectId(userid),
-            latitude: latitude,
-            longitude: longitude,
-            altitude: altitude,
-            accuracy: accuracy,
-            altitudeAccuracy: altitudeAccuracy,
-            heading: heading,
-            speed: speed,
-            timestamp: Date.now(),
-            expireAt: new Date(Date.now() + (1000 * 60 * 60 * 24 * 30))
-        };
-
-        try {
-            db.beep().collection("location").createIndex({ expireAt: 1 }, { expireAfterSeconds: 0 });
-            db.beep().collection("location").insertOne(dataToInsert);
-        } 
-        catch (error) {
-            Sentry.captureException(error);
-            console.log(error);
-        }
+        clients.delete(userId);
     });
 });
 
